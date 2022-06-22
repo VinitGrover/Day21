a64b201c36dc3586442b45401c4817043bc4e8ac
VinitGrover <vinitgrover255@gmail.com>
2022-06-22 17:48:25 +0530

    Added MoodAnalyser Program

---
+++ src/test/java/com/bridgelabz/TestMoodAnalyser.java
@@ -0,0 +1,39 @@
+package com.bridgelabz;
+
+import org.junit.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class TestMoodAnalyser {
+
+    @Test
+    public void givenSadMood_whenTested_shouldReturnSad() {
+        MoodAnalyser mood = new MoodAnalyser();
+        String result = mood.analyseMood("I am in Sad mood");
+        assertEquals(result, "Sad");
+    }
+
+    @Test
+    public void givenAnyMood_whenTested_shouldReturnHappy() {
+        MoodAnalyser mood = new MoodAnalyser();
+        String result = mood.analyseMood("I am in any mood");
+        assertEquals(result, "Happy");
+    }
+
+    @Test
+    public void givenCallingNoParamMethod_whenTested_shouldReturnSad() {
+        MoodAnalyser mood = new MoodAnalyser("I am in Sad mood");
+        String result = mood.analyseMood();
+        assertEquals(result, "Sad");
+    }
+
+    @Test
+    public void givenCallingNoParamMethod_whenTested_shouldReturnHappy() {
+        MoodAnalyser mood = new MoodAnalyser("I am in Happy mood");
+        String result = mood.analyseMood();
+        assertEquals(result, "Happy");
+    }
+
+
+
+}
